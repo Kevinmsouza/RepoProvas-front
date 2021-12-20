4313fa66b25e9c40e46bac508a3e3b15080c85e5
Kevinmsouza <kevinm.souza@hotmail.com>
2021-12-20 07:18:57 -0400

    added link to prod deploy

--- src/services/repoprovas.js
+++ src/services/repoprovas.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL === "prod" ? "" : "http://localhost:4000";
+const BASE_URL = process.env.REACT_APP_API_URL === "prod" ? "https://kms-repoprovas-back.herokuapp.com" : "http://localhost:4000";
 
 function getCategories() {
     return axios.get(`${BASE_URL}/categories`);
